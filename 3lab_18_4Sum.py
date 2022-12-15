5ea40ca8d1231676cbf9d24d2fb31d60e7fa3664
NastyaVlasenkova <116723521+NastyaVlasenkova@users.noreply.github.com>
2022-12-15 13:20:28 +0300

    Update 3lab_18_4Sum.py

--- 3lab_18_4Sum.py
+++ 3lab_18_4Sum.py
@@ -8,34 +8,27 @@ Task: Учитывая массив nums из n целых чисел,
     nums[a] + nums[b] + nums[c] + nums[d] == target
 '''
 
-class Solution(object):
+class Solution:
     def fourSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        nums, result, lookup = sorted(nums), [], {}
-        for i in range(0, len(nums) - 1):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] not in lookup:
-                    lookup[nums[i] + nums[j]] = []
-                is_duplicated = False
-                for [x, y] in lookup[nums[i] + nums[j]]:
-                    if nums[x] == nums[i]:
-                        is_duplicated = True
-                        break
-                if not is_duplicated:
-                    lookup[nums[i] + nums[j]].append([i, j])
-        ans = {}
-        for c in range(2, len(nums)):
-            for d in range(c+1, len(nums)):
-                if target - nums[c] - nums[d] in lookup:
-                    for [a, b] in lookup[target - nums[c] - nums[d]]:
-                        if b < c:
-                            quad = [nums[a], nums[b], nums[c], nums[d]]
-                            quad_hash = " ".join(str(quad))
-                            if quad_hash not in ans:
-                                ans[quad_hash] = True
-                                result.append(quad)
-        return result
+        n = len(nums)
+        nums.sort()
+        res = set()
+        for i in range(n):
+            a = nums[i]
+            for j in range(i + 1, n - 2):
+                b = nums[j]
+                left = j + 1
+                right = n - 1
+                while left < right:
+                    c = nums[left]
+                    d = nums[right]
+                    total = b + c + d
+                    if total == target - a:
+                        res.add(tuple([a, b, c, d]))
+                        left += 1
+                        right -= 1
+                    elif total > target - a:
+                        right -= 1
+                    else:
+                        left += 1
+        return list(res)
